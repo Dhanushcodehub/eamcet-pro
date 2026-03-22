ec215445080ba57d46c7a6ca408b9ccf2ed8ee46
Dhanushcodehub <Dhanushmathe@gmail.com>
2026-03-23 01:03:49 +0530

    fix: move Firebase config to env variables and update gitignore for security

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 
 // Firebase Project config
 const firebaseConfig = {
-  apiKey: "AIzaSyClgvskceBwQhZEU1X7E2ZmVs_z3AIv_XE",
-  authDomain: "eamcet-pro.firebaseapp.com",
-  projectId: "eamcet-pro",
-  storageBucket: "eamcet-pro.firebasestorage.app",
-  messagingSenderId: "846356712669",
-  appId: "1:846356712669:web:55a7f762a05978e9d05f34",
-  measurementId: "G-C6LFX14TXN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
